736cca30f506dbcd1eed15c87ed80ef2eebdafa8
0MAVM0 <ediconpts9@gmail.com>
2025-04-04 21:36:01 +0500

    CustomUser | Model

--- config/user/models.py
+++ config/user/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    birth_date = models.DateTimeField(auto_now=True, null=True, blank=True, verbose_name="Birth Date")
+    address = models.CharField(max_length=255, null=True, blank=True, verbose_name="Address")
+    phone = models.IntegerField(null=True, blank=True, verbose_name="Phone Number")
+    avatar = models.ImageField(upload_to="avatars/", null=True, blank=True, verbose_name="Avatar")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"{self.username} - {self.phone} - {self.address}"
