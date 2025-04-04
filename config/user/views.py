f2351c04e0a7cd4dced64f8fcc9699019cb896dd
0MAVM0 <ediconpts9@gmail.com>
2025-04-04 21:55:44 +0500

    view.py - sing_up

--- config/user/views.py
+++ config/user/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth.hashers import make_password
+from django.shortcuts import render, redirect
+from .models import CustomUser
+from .forms import UserForm
 
-# Create your views here.
+def sign_up(request):
+    form = UserForm
+
+    if request.method == "POST":
+        form = UserForm(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.password = make_password(form.cleaned_data["password1"])
+            form.save()
+
+            return redirect("home")
+    else:
+        form = UserForm()
+    
+    context = { "form" : form }
+
+    return render(request, "user/signup.html", context)
