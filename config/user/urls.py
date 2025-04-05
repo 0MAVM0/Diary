6dbb5f3c6217696e2426453f66ad32c0920a7990
0MAVM0 <ediconpts9@gmail.com>
2025-04-05 18:41:45 +0500

    Log In - View, Template & Form

--- config/user/urls.py
+++ config/user/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns= [
     path("", home_page, name="home"),
     path("sign_up/", SignUpView.as_view(), name="sign_up"),
+    path("log_in/", log_in, name="log_in"),
 ]
--- config/user/views.py
+++ config/user/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
 from .models import CustomUser
 from django.views import View
-from .forms import UserCreationForm
+from .forms import *
 
 def home_page(request):
     return render(request, "home.html")
@@ -37,7 +38,7 @@ class SignUpView(View):
             user.password = make_password(form.cleaned_data["password1"])
             form.save()
 
-            return redirect("login")
+            return redirect("log_in")
 
         context = { "form" : form }
 
@@ -49,3 +50,46 @@ class SignUpView(View):
         context = { "form" : form }
 
         return render(request, "user/sign_up.html", context)
+
+def log_in(request):
+    if request.method == "POST":
+        form = UserLoginForm(request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+
+                return redirect("home")
+            else:
+                form.add_error("Doesn't Exist")
+    else:
+        form = UserLoginForm()
+    
+    context = { "form" : form }
+
+    return render(request, "user/log_in.html", context)
+
+'''
+class LogInView(View):
+    def post(self, request):
+        form = UserLoginForm(request.POST)
+
+        if form.is_valid():
+            return redirect("home")
+
+        context = { "form" : form }
+
+        return render(request, "user/log_in.html", context)
+    
+    def get(self, request):
+        form = UserLoginForm()
+
+        context = { "form" : form }
+
+        return render(request, "user/log_in.html", context)
+'''
