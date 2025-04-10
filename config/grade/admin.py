6309ca768bba825b23260287b56958bae6b5bf33
0MAVM0 <ediconpts9@gmail.com>
2025-04-10 18:12:59 +0500

    Grade | Models - Clas, Subject & Grade | admin.py

--- config/grade/admin.py
+++ config/grade/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(Clas)
+class ClasAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+    list_filter = ["id", "name"]
+    search_fields = ["name"]
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ["id", "subject", "clas"]
+    list_filter = ["id", "subject"]
+    search_fields = ["subject", "clas"]
+
+@admin.register(Grade)
+class GradeAdmin(admin.ModelAdmin):
+    list_display = ["id", "subject", "student", "grade", "created_at"]
+    list_filter = ["id", "subject", "student"]
+    search_fields = ["student"]
--- config/grade/models.py
+++ config/grade/models.py
@@ -1,3 +1,25 @@
+from user.models import CustomUser
 from django.db import models
 
-# Create your models here.
+
+class Clas(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name="Class")
+
+    def __str__(self) -> str:
+        return self.name
+
+class Subject(models.Model):
+    subject = models.CharField(max_length=255, null=False, blank=False, verbose_name="Subject's Name")
+    clas = models.ForeignKey(Clas, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.subject}"
+
+class Grade(models.Model):
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name="Grades")
+    student = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="Grades")
+    grade = models.IntegerField(default=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"{self.student} - {self.Subject} - {self.grade}"
