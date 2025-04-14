549cb9f1a94e845ca1c08e927cfdcd7918b4f2b5
0MAVM0 <ediconpts9@gmail.com>
2025-04-14 21:48:54 +0500

    Deleted Unnecessary Codes

--- config/grade/urls.py
+++ config/grade/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("subjects/", subjects, name="subjects"),
-    path("students/", students, name="students"),
-    path("classes/", classes, name="classes"),
     path("student/<int:id>/", grades_of_student, name="grades_of_a_student"),
     path("subject/<int:id>/", grades_in_subject, name="grades_in_a_subject"),
 ]
--- config/grade/views.py
+++ config/grade/views.py
@@ -2,24 +2,6 @@ from django.shortcuts import render
 from user.models import CustomUser
 from .models import *
 
-def subjects(request):
-    subjects = Subject.objects.all()
-    context = { "subjects" : subjects }
-
-    return render(request, "subjects.html", context)
-
-def students(request):
-    students = CustomUser.objects.all()
-    context = { "students" : students }
-
-    return render(request, "students.html", context)
-
-def classes(request):
-    classes = Clas.objects.all()
-    context = { "classes" : classes }
-
-    return render(request, "classes.html", context)
-
 def grades_of_student(request, id):
     student = CustomUser.objects.filter(id=id).first()
     grades = Grade.objects.filter(student=student)
